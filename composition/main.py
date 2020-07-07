ad925851737bd3ce82529164a85d6484dbdbc5e2
Rafael Henrique <oraffaudi@gmail.com>
2020-07-07 12:12:50 -0300

    Alterando a role do gerente em tempo de execuçao

--- composition/main.py
+++ composition/main.py
@@ -1,4 +1,16 @@
-from hr import PayrollSystem
+# from hr import PayrollSystem
+# from productivity import ProductivitySystem
+# from employees import EmployeeDatabase
+
+# productivity_system = ProductivitySystem()
+# payroll_system = PayrollSystem()
+# employee_database = EmployeeDatabase()
+# employees = employee_database.employees
+# productivity_system.track(employees, 40)
+# payroll_system.calculate_payroll(employees)
+
+
+from hr import PayrollSystem, HourlyPolicy
 from productivity import ProductivitySystem
 from employees import EmployeeDatabase
 
@@ -6,5 +18,8 @@ productivity_system = ProductivitySystem()
 payroll_system = PayrollSystem()
 employee_database = EmployeeDatabase()
 employees = employee_database.employees
+manager = employees[0]
+manager.payroll = HourlyPolicy(55)
+
 productivity_system.track(employees, 40)
 payroll_system.calculate_payroll(employees)
