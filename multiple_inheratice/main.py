65bf10d4fc4cb86f08f21a009b503e17a5c37bf4
Rafael Henrique <oraffaudi@gmail.com>
2020-07-07 01:13:30 -0300

    Refactoring para evitar erro diamente com multipla herança

--- multiple_inheratice/main.py
+++ multiple_inheratice/main.py
@@ -1,18 +1,33 @@
+# import hr
+# import employees
+# import productivity
+
+# manager = employees.Manager(1, 'Mary Poppins', 3000)
+# secretary = employees.Secretary(2, 'John Smith', 1500)
+# sales_guy = employees.SalesPerson(3, 'Kevin Bacon', 1000, 250)
+# factory_worker = employees.FactoryWorker(2, 'Jane Doe', 40, 15)
+# employees = [
+#     manager,
+#     secretary,
+#     sales_guy,
+#     factory_worker,
+# ]
+# productivity_system = productivity.ProductivitySystem()
+# productivity_system.track(employees, 50)
+# payroll_system = hr.PayrollSystem()
+# payroll_system.calculate_payroll(employees)
+
+
 import hr
 import employees
 import productivity
 
-manager = employees.Manager(1, 'Mary Poppins', 3000)
-secretary = employees.Secretary(2, 'John Smith', 1500)
-sales_guy = employees.SalesPerson(3, 'Kevin Bacon', 1000, 250)
-factory_worker = employees.FactoryWorker(2, 'Jane Doe', 40, 15)
-employees = [
-    manager,
-    secretary,
-    sales_guy,
-    factory_worker,
-]
+manager = employees.Manager(1, "Mary Poppins", 3000)
+secretary = employees.Secretary(2, "John Smith", 1500)
+sales_guy = employees.SalesPerson(3, "Kevin Bacon", 1000, 250)
+factory_worker = employees.FactoryWorker(4, "Jane Doe", 40, 15)
+company_employees = [manager, secretary, sales_guy, factory_worker]
 productivity_system = productivity.ProductivitySystem()
-productivity_system.track(employees, 50)
+productivity_system.track(company_employees, 40)
 payroll_system = hr.PayrollSystem()
-payroll_system.calculate_payroll(employees)
+payroll_system.calculate_payroll(company_employees)
