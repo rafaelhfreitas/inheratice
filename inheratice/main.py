7f258a8b0422c34c172075aca0e8da7ffc175b71
Rafael Henrique <oraffaudi@gmail.com>
2020-07-06 23:35:36 -0300

    Reorganizaçao dos modulos

--- inheratice/main.py
+++ inheratice/main.py
@@ -11,27 +11,6 @@ payroll_system.calculate_payroll([
     commission_employee
 ])
 
-
-import hr
-import employees
-import productivity
-
-manager = employees.Manager(1, 'Mary Poppins', 3000)
-secretary = employees.Secretary(2, 'John Smith', 1500)
-sales_guy = employees.SalesPerson(3, 'Kevin Bacon', 1000, 250)
-factory_worker = employees.FactoryWorker(2, 'Jane Doe', 40, 15)
-employees = [
-    manager,
-    secretary,
-    sales_guy,
-    factory_worker,
-]
-productivity_system = productivity.ProductivitySystem()
-productivity_system.track(employees, 50)
-payroll_system = hr.PayrollSystem()
-payroll_system.calculate_payroll(employees)
-
-
 import hr
 import employees
 import disgruntled
@@ -47,3 +26,4 @@ payroll_system.calculate_payroll([
     commission_employee,
     disgruntled_employee
 ])
+
